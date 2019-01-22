357c39a15f4824d9c2296ea0ef3c5754b80ab044
ganchoiliev <gancho.iliev92@gmail.com>
2019-01-22 13:21:58 +0000

    Sass and JS working on the menu

---
+++ dist/js/main.js
@@ -0,0 +1,8 @@
+// Select DOM Items
+const menuBtn = document.querySelector(".btn-menu");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-items");
+
+// Set Initial State Of Menu
